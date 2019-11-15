15e169ee71dae966c4fe6bcad16e7147d46aaeed
Pitrified <nobilipietro@gmail.com>
2019-11-15 21:08:28 +0100

    Add reworked car image creation
    Much better IMHO :)

--- gym_racer/envs/racer_car.py
+++ gym_racer/envs/racer_car.py
@@ -1,6 +1,8 @@
 import logging
 import numpy as np
 
+from math import ceil
+
 import pygame
 from pygame.sprite import Sprite
 from pygame.transform import rotate
@@ -29,13 +31,18 @@ class RacerCar(Sprite):
         # viscous drag coefficient
         self.drag_coeff = 0.5
 
+        # generate the car image and create all rotated versions
         self._create_car_image()
         self._rotate_car_image()
 
-        # do a nop to init the correct image and rect of the sprite
-        self.step("nop")
+        # pick the correct image and place it
+        self.image = self.rot_car_image[self.direction]
+        self.rect = self.rot_car_rect[self.direction]
+        self.rect.center = self.pos_x, self.pos_y
 
     def step(self, action):
+        """Perform the action
+        """
 
         # pick the rotated image and place it
         self.image = self.rot_car_image[self.direction]
@@ -50,64 +57,85 @@ class RacerCar(Sprite):
         logg = logging.getLogger(f"c.{__name__}._create_car_image")
         logg.debug(f"Start _create_car_image")
 
-        car_size = (60, 60)
-        car_surf = Surface(car_size)
+        # wheel dimensions
+        w_color = (80, 80, 80, 0)
+        w_len = 7  # horizontal length of the wheel
+        w_radius = 3
+        w_wid = w_radius * 2
+        delta = -3  # space from car to wheel
+
+        # car dimensions
+        car_wid = 20
+        car_len = 30
+        # place the car so that it touches the border of the surf
+        car_top = w_radius
+        car_left = ceil(car_wid / 2)
+
+        # create a surf just big enough for the car
+        car_surf_size = (car_len + car_wid, car_wid + w_wid)
+        car_surf = Surface(car_surf_size)
 
         black = (0, 0, 0)
         car_surf.fill(black)
         # black colors will not be blit
-        car_surf.set_colorkey(black)
-
-        wheel_color = (80, 80, 80, 0)
-        wheel_len = 7
-        wheel_wid = 6
-        mid = wheel_wid // 2
-        delta = -3
+        #  car_surf.set_colorkey(black)
+        # RLEACCEL should make blitting faster
+        car_surf.set_colorkey(black, pygame.RLEACCEL)
+        # show the surface area to debug
+        #  car_surf.fill((0, 0, 255))
 
         # top left wheel
-        self._draw_oval(
-            car_surf, 15 + delta + mid, 10 - mid, wheel_wid, wheel_len, wheel_color
-        )
+        w_top = car_top - w_radius
+        w_left = car_left + delta + w_radius
+        self._draw_oval(car_surf, w_top, w_left, w_wid, w_len, w_color)
+
         # top right wheel
-        self._draw_oval(
-            car_surf,
-            15 + 30 - (delta + mid + wheel_len),
-            10 - mid,
-            wheel_wid,
-            wheel_len,
-            wheel_color,
-        )
-        # bottom left
-        self._draw_oval(
-            car_surf, 15 + delta + mid, 30 - mid, wheel_wid, wheel_len, wheel_color
-        )
+        w_top = car_top - w_radius
+        w_left = car_left + car_len - (delta + w_radius + w_len)
+        self._draw_oval(car_surf, w_top, w_left, w_wid, w_len, w_color)
+
+        # bottom left wheel
+        w_top = car_top + car_wid - w_radius
+        w_left = car_left + delta + w_radius
+        self._draw_oval(car_surf, w_top, w_left, w_wid, w_len, w_color)
+
         # bottom right wheel
-        self._draw_oval(
-            car_surf,
-            15 + 30 - (delta + mid + wheel_len),
-            30 - mid,
-            wheel_wid,
-            wheel_len,
-            wheel_color,
-        )
+        w_top = car_top + car_wid - w_radius
+        w_left = car_left + car_len - (delta + w_radius + w_len)
+        self._draw_oval(car_surf, w_top, w_left, w_wid, w_len, w_color)
 
         # body
-        body_color = (255, 0, 0, 0)
-        #  self._draw_oval(car_surf, 15, 10, 20, 30, body_color)
-        self._draw_oval(car_surf, 19, 8, 20, 30, body_color)
+        body_color = (255, 0, 0, 128)
+        self._draw_oval(car_surf, car_top, car_left, car_wid, car_len, body_color)
+
+        # windshield
+        wind_wid1 = 4
+        wind_wid2 = 7
+        # vertical mid point
+        wind_mid = car_top + car_wid // 2
+        # horizontal points 52 46 36 36 46
+        wind_hpos = car_len - 2
+        d1 = 10
+        d2 = 16
+        wind_points = [
+            (wind_hpos + d2, wind_mid - 1),
+            (wind_hpos + d1, wind_mid - wind_wid2),
+            (wind_hpos, wind_mid - wind_wid1),
+            (wind_hpos, wind_mid + wind_wid1 - 1),
+            (wind_hpos + d1, wind_mid + wind_wid2 - 1),
+            (wind_hpos + d2, wind_mid),
+        ]
+        wind_color = (0, 255, 255, 0)
+        pygame.draw.polygon(car_surf, wind_color, wind_points)
 
         self.orig_image = car_surf
 
     def _draw_oval(self, surf, top, left, width, length, color):
-        """draw an oval, top left is for the rectangle
+        """draw an oval on Surface surf
 
-        circle rect circle
-        draw.rectangle(((15, 10), (45, 30)), fill="red")
-        draw.ellipse(((5, 10), (25, 30)), fill="red")
-        draw.ellipse(((35, 10), (55, 30)), fill="red")
-        top 15 left 10 width 20 length 30
-
-        Rect( left, top, width, height )
+        horizontal circle-rect-circle 
+        top left is for the rectangle
+        width is the height, length is the width lol
         """
         # make width even to simplify things
         if width % 2 != 0:
@@ -119,6 +147,7 @@ class RacerCar(Sprite):
         pygame.draw.circle(surf, color, w_pos, w_size)
         w_pos = (left + length, top + mid)
         pygame.draw.circle(surf, color, w_pos, w_size)
+        # constructor Rect( left, top, width, height )
         w_rect = (left, top, length, width)
         pygame.draw.rect(surf, color, w_rect)
 
