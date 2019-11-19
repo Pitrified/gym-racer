bb8e82ecc51502c09654551ed4f8cea5b3904914
Pitrified <nobilipietro@gmail.com>
2019-11-19 10:41:39 +0100

    Prepare for PyPI publishing

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 # define the name to import the env
 # import gym_racer
-setup(name="gym_racer", version="0.0.1", install_requires=["gym", "pygame", "numpy"])
+setup(
+    name="gym_racer",
+    version="0.0.1",
+    author="Pitrified",
+    author_email="pitrified.git@gmail.com",
+    description="OpenAI gym environment of a racing car.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pitrified/gym-racer",
+    packages=setuptools.find_packages(where="."),
+    install_requires=["gym", "pygame", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
